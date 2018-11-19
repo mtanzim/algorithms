9536d8e6e2cd684f3b96bd1f5811da0c46454fc7
Tanzim Mokammel <mtanzim@gmail.com>
2018-11-19 14:22:46 +0100

    added implementation for bag data structure

---
+++ herlper/Bag.ts
@@ -0,0 +1,69 @@
+
+interface Iterable <T> {
+  hasNext() : boolean;
+  next() : T;
+  resetPosition() : void;
+}
+
+
+export default class Bag <T> implements Iterable <T> {
+
+  // prevent modifying bag
+  private bag: Array<T> ;
+  private position: number;
+
+  constructor() {
+    this.bag = [];
+    this.position = 0;
+  }
+
+  add(item:T) {
+    this.bag.push(item);
+  }
+
+  isEmpty(): boolean {
+    return (this.bag.length === 0);
+  }
+
+  getSize(): number {
+    return this.bag.length;
+  }
+
+  next(): T {
+    let item = this.bag[this.position];
+    this.position ++;
+    return item;
+  }
+
+  resetPosition() {
+    this.position = 0;
+  }
+
+  hasNext() : boolean {
+    return (this.position !== this.bag.length);
+  }
+
+}
+
+function driver () : void {
+  
+  let bag = new Bag();
+  bag.add(2);
+  bag.add(['a',2,5]);
+  bag.add('Hello');
+  bag.add({text:'Hi', num:45});
+  
+  console.log(bag);
+  
+  console.log(bag.hasNext() && bag.next());
+  console.log(bag.hasNext() && bag.next());
+  console.log(bag.hasNext() && bag.next());
+  console.log(bag.hasNext() && bag.next());
+  console.log(bag.hasNext() && bag.next());
+  bag.resetPosition();
+  console.log(bag.hasNext() && bag.next());
+  
+}
+
+
+// driver();
